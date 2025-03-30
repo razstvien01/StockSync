b6981d4a430daf370509b53a3eeb3e336b7bf2ba
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-03-31 00:22:47 +0800

    added dotnetenv package

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,4 +1,11 @@
+using DotNetEnv;
+
+Env.Load();
+
 var builder = WebApplication.CreateBuilder(args);
+var urls = Environment.GetEnvironmentVariable("ASPNETCORE_URLS") ?? "http://localhost:5000";
+
+builder.WebHost.UseUrls(urls);
 
 builder.Services.AddOpenApi();
 
