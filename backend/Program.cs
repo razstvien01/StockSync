abd3ba8915c4cd928805f0edcbc50d69980d01bf
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-03-31 00:31:47 +0800

    updated connectionstring

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,13 +1,26 @@
+using backend.data;
 using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
 
 Env.Load();
 
-var builder = WebApplication.CreateBuilder(args);
 var urls = Environment.GetEnvironmentVariable("ASPNETCORE_URLS") ?? "http://localhost:5000";
 
-builder.WebHost.UseUrls(urls);
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST") ?? "";
+var dbPort = Environment.GetEnvironmentVariable("DB_PORT") ?? "";
+var dbName = Environment.GetEnvironmentVariable("DB_NAME") ?? "";
+var dbUser = Environment.GetEnvironmentVariable("DB_USER") ?? "";
+var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD") ?? "";
+var connectionSring = $"Host={dbHost};Port={dbPort};Database={dbName};Username={dbUser};Password={dbPassword}";
 
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.UseUrls(urls);
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<AppDBContext>(options =>
+{
+    options.UseNpgsql(connectionSring);
+});
 
 var app = builder.Build();
 
