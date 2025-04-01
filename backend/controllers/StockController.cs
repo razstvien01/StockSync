f29eeae9244f050fb923f1f2ae5ca28c385e3960
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-04-01 14:10:43 +0800

    implemented StockController with GetAllStocks functionality

--- backend/controllers/StockController.cs
+++ backend/controllers/StockController.cs
@@ -3,16 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using backend.data;
+using backend.mappers;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace backend.controllers
 {
+    [Route("api/stock")]
+    [ApiController]
     public class StockController: ControllerBase
     {
         private readonly AppDBContext _context;
         public StockController(AppDBContext context)
         {
             _context = context;
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> GetAllStock(){
+            var stocks = await _context.Stocks.ToListAsync();
+            var stockDto = stocks.Select(s => s.ToStockDto());
+            
+            return Ok(stockDto);
         }
     }
 }
