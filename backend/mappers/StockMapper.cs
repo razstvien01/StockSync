3f1300af869736701b0e25e6f4a59e5dc4bae5ba
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-04-01 14:00:01 +0800

    added stock mapper

--- backend/mappers/StockMapper.cs
+++ backend/mappers/StockMapper.cs
@@ -2,10 +2,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using backend.dtos.Stock;
+using backend.models;
 
 namespace backend.mappers
 {
-    public class StockMapper
+    public static class StockMapper
     {
+        public static StockDto ToStockDto(this Stock stockModel)
+        {
+            return new StockDto
+            {
+                Id = stockModel.Id,
+                Symbol = stockModel.Symbol,
+                CompanyName = stockModel.CompanyName,
+                Purchase = stockModel.Purchase,
+                LastDividend = stockModel.LastDividend,
+                Industry = stockModel.Industry,
+                MarketCap = stockModel.MarketCap,
+                CreatedAt = stockModel.CreatedAt
+            };
+        }
     }
 }
