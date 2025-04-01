c39aa4e1d0e6ef8ee9adfd3116711b06183e281b
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-04-02 00:19:00 +0800

    added getstockbyid api function

--- backend/controllers/StockController.cs
+++ backend/controllers/StockController.cs
@@ -11,20 +11,32 @@ namespace backend.controllers
 {
     [Route("api/stock")]
     [ApiController]
-    public class StockController: ControllerBase
+    public class StockController : ControllerBase
     {
         private readonly AppDBContext _context;
         public StockController(AppDBContext context)
         {
             _context = context;
         }
-        
+
         [HttpGet]
-        public async Task<IActionResult> GetAllStock(){
+        public async Task<IActionResult> GetAllStocks()
+        {
             var stocks = await _context.Stocks.ToListAsync();
             var stockDto = stocks.Select(s => s.ToStockDto());
-            
+
             return Ok(stockDto);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetStockById([FromRoute] int id)
+        {
+            var stock = await _context.Stocks.FindAsync(id);
+            if (stock == null)
+            {
+                return NotFound();
+            }
+            return Ok(stock);
         }
     }
 }
