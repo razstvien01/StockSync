5e6176b394d385fa89f6f9d46f4a68ba5fe7cacf
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-04-03 00:33:11 +0800

    added dto for the update stock req

---
+++ backend/dtos/Stock/UpdateStockRequestDto.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.dtos.Stock
+{
+    public class UpdateStockRequestDto
+    {
+        public string Symbol { get; set; } = string.Empty;
+        public string  CompanyName { get; set; } = string.Empty;
+        public decimal Purchase { get; set; }
+        public decimal LastDividend { get; set; }
+        public string Industry { get; set; } = string.Empty;
+        public long MarketCap { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
