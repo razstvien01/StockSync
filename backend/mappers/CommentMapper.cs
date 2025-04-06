b84f4eb0e715c61b9fa75a0ad5bf4580906a3d50
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-04-06 21:52:25 +0800

    implemented comment mapper

--- backend/mappers/CommentMapper.cs
+++ backend/mappers/CommentMapper.cs
@@ -2,11 +2,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using backend.dtos.Comment;
+using backend.models;
 
 namespace backend.mappers
 {
-    public class CommentMapper
+    public static class CommentMapper
     {
+        public static CommentDto ToCommentDto(this Comment dto)
+        {
+            return new CommentDto
+            {
+                Id = dto.Id,
+                Title = dto.Title,
+                Content = dto.Content,
+                CreatedAt = dto.CreatedAt,
+                StockId = dto.StockId
+            };
+        }
         
+        public static Comment ToCommentFromCreateDto(this CreateCommentRequestDto dto){
+            return new Comment{
+                Title = dto.Title,
+                Content = dto.Content,
+                CreatedAt = dto.CreatedAt,
+                StockId = dto.StockId
+            };
+        }
     }
 }
