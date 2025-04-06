960b38a811767cc5d5fee88f5642b16500e7c3b8
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-04-06 21:52:06 +0800

    added create comment reqeust dto class

---
+++ backend/dtos/Comment/CreateCommentRequestDto.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.dtos.Comment
+{
+    public class CreateCommentRequestDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public int? StockId { get; set; }
+    }
+}
