19b31881148eaaea493b351be2bd1323a2684e4a
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-04-13 22:19:15 +0800

    added dto annotations

--- backend/dtos/Comment/UpdateCommentRequestDto.cs
+++ backend/dtos/Comment/UpdateCommentRequestDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,13 @@ namespace backend.dtos.Comment
 {
     public class UpdateCommentRequestDto
     {
+        [Required]
+        [MinLength(5, ErrorMessage = "Title must be at least 5 characters long.")]
+        [MaxLength(100, ErrorMessage = "Title must be at most 100 characters long.")]
         public string Title { get; set; } = string.Empty;
+        [Required]
+        [MinLength(5, ErrorMessage = "Content must be at least 5 characters long.")]
+        [MaxLength(500, ErrorMessage = "Content must be at most 500 characters long.")]
         public string Content { get; set; } = string.Empty;
     }
 }
--- backend/dtos/Stock/CreateStockRequestDto.cs
+++ backend/dtos/Stock/CreateStockRequestDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,26 @@ namespace backend.dtos.Stock
 {
     public class CreateStockRequestDto
     {
+        [Required]
+        [MinLength(1, ErrorMessage = "Symbol must be at least 1 character long.")]
+        [MaxLength(10, ErrorMessage = "Symbol must be at most 10 characters long.")]
         public string Symbol { get; set; } = string.Empty;
-        public string  CompanyName { get; set; } = string.Empty;
+        [Required]
+        [MinLength(1, ErrorMessage = "Company name must be at least 1 character long.")]
+        [MaxLength(15, ErrorMessage = "Company name must be at most 15 characters long.")]
+        public string CompanyName { get; set; } = string.Empty;
+        [Required]
+        [Range(1, 1000000000, ErrorMessage = "Price must be between 1 and 1,000,000,000.")]
         public decimal Purchase { get; set; }
+        [Required]
+        [Range(0.001, 100, ErrorMessage = "Dividend must be between 0.001 and 100.")]
         public decimal LastDividend { get; set; }
+        [Required]
+        [MinLength(1, ErrorMessage = "Industry must be at least 1 character long.")]
+        [MaxLength(10, ErrorMessage = "Industry must be at most 10 characters long.")]
         public string Industry { get; set; } = string.Empty;
+        [Required]
+        [Range(1, 5000000000000, ErrorMessage = "Market cap must be between 1 and 5,000,000,000,000.")]
         public long MarketCap { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
