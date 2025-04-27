8b340d61487c53ec10bba2a1691bbb9f23464349
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-04-27 17:45:16 +0800

    implemented OnModelCreating

--- backend/data/AppDBContext.cs
+++ backend/data/AppDBContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using backend.models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,5 +17,25 @@ namespace backend.data
 
         public DbSet<Stock> Stocks { get; set; }
         public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            List<IdentityRole> roles = new List<IdentityRole>{
+                new IdentityRole {
+                    Id = "1",
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole {
+                    Id = "2",
+                    Name = "User",
+                    NormalizedName = "USER"
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
     }
 }
