9b9f2b13032262fd93da502b8898bb63d9dcaa1b
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-05-04 17:49:30 +0800

    modified the email to username

--- backend/dtos/Account/LoginDto.cs
+++ backend/dtos/Account/LoginDto.cs
@@ -9,7 +9,7 @@ namespace backend.dtos.Account
     public class LoginDto
     {
         [Required]
-        public string Email { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
         [Required]
         public string Password { get; set; } = string.Empty;
     }
