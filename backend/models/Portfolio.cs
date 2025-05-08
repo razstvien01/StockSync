671cf99cf6d7875190ed4e213a927c649b690d93
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-05-08 19:35:05 +0800

    added Portfolio, for the connection of Stocks and the User (many-to-many)

---
+++ backend/models/Portfolio.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.models
+{
+    [Table("Portfolio")]
+    public class Portfolio
+    {
+        public required int AppUserId { get; set; }
+        public int StockId { get; set; }
+        public required AppUser AppUser { get; set; }
+        public required Stock Stock { get; set; }
+    }
+}
