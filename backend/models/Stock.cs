6c4f65475ab2203cf61759ecee7da756207ae517
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-05-08 19:37:25 +0800

    added Portfolio navigator variable

--- backend/models/Stock.cs
+++ backend/models/Stock.cs
@@ -12,14 +12,15 @@ namespace backend.models
     {
         public int Id { get; set; }
         public string Symbol { get; set; } = string.Empty;
-        public string  CompanyName { get; set; } = string.Empty;
-        [Column (TypeName = "decimal(18, 2)")]
+        public string CompanyName { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Purchase { get; set; }
-        [Column (TypeName = "decimal(18,2)")]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal LastDividend { get; set; }
         public string Industry { get; set; } = string.Empty;
         public long MarketCap { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public List<Comment> Comments { get; set; } = new List<Comment>();
+        public List<Portfolio> Portfolios { get; set; } = new List<Portfolio>();
     }
 }
