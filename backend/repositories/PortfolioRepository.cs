b249e5e2acc3daaad7ed3014ad6a8ac8bc1259ba
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-05-10 21:00:18 +0800

    implemented portfolio repo, implemented getuserportfolio repo func

---
+++ backend/repositories/PortfolioRepository.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using backend.data;
+using backend.interfaces;
+using backend.models;
+
+namespace backend.repositories
+{
+    public class PortfolioRepository : IPortfolioRepository
+    {
+        private readonly AppDBContext _context;
+        public PortfolioRepository(AppDBContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Stock>> GetUserPortfolio(AppUser user)
+        {
+            return await _context.Portfolios.Where(u => u.AppUserId == user.Id)
+                .Select(p => new Stock
+                {
+                    Id = p.StockId,
+                    Symbol = p.Stock.Symbol,
+                    CompanyName = p.Stock.CompanyName,
+                    Purchase = p.Stock.Purchase,
+                    LastDividend = p.Stock.LastDividend,
+                    Industry = p.Stock.Industry,
+                    MarketCap = p.Stock.MarketCap
+                }).ToListAsync();
+        }
+    }
+}
