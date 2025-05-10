261a6239969415b1511c8519316348c98b02f959
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-05-10 21:52:46 +0800

    added error handling in jwt_claim_uri

--- backend/extensions/ClaimExtensions.cs
+++ backend/extensions/ClaimExtensions.cs
@@ -12,9 +12,14 @@ namespace backend.extensions
         public static string? GetUserName(this ClaimsPrincipal user)
         {
             Env.Load();
-            
+
             var JWT_CLAIMS_URI = Environment.GetEnvironmentVariable("JWT_CLAIMS_URI") ?? "";
-            
+
+            if (string.IsNullOrEmpty(JWT_CLAIMS_URI))
+            {
+                throw new InvalidOperationException("JWT Claims URI is not set in environment variables.");
+            }
+
             return user?.Claims?
                 .FirstOrDefault(c => c.Type == JWT_CLAIMS_URI)
                 ?.Value;
