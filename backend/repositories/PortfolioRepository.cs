b08d1dd963d743355f3f5a07559de39d829372db
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-05-12 22:46:59 +0800

    implemented function logic for CreateAsync repo

--- backend/repositories/PortfolioRepository.cs
+++ backend/repositories/PortfolioRepository.cs
@@ -16,13 +16,21 @@ namespace backend.repositories
         {
             _context = context;
         }
+
+        public async Task<Portfolio> CreateAsync(Portfolio portfolio)
+        {
+            await _context.Portfolios.AddAsync(portfolio);
+            await _context.SaveChangesAsync();
+            return portfolio;
+        }
+
         public async Task<List<Stock>> GetUserPortfolio(AppUser user)
         {
             return await _context.Portfolios.Where(u => u.AppUserId == user.Id)
                 .Select(p => new Stock
                 {
                     Id = p.StockId,
-                    Symbol = p.Stock.Symbol,
+                    Symbol = p.Stock!.Symbol,
                     CompanyName = p.Stock.CompanyName,
                     Purchase = p.Stock.Purchase,
                     LastDividend = p.Stock.LastDividend,
