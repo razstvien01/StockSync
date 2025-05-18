107e871a99e3dd0cd707dd6072f04e83903984e3
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-05-18 13:15:37 +0800

    added DeletePortfolio abstract func

--- backend/interfaces/IPortfolioRepository.cs
+++ backend/interfaces/IPortfolioRepository.cs
@@ -10,5 +10,6 @@ namespace backend.interfaces
     {
         Task<List<Stock>> GetUserPortfolio(AppUser user);
         Task<Portfolio> CreateAsync(Portfolio portfolio);
+        Task<Portfolio?> DeletePortfolio(AppUser user, string symbol);
     }
 }
