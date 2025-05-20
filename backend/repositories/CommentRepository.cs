c9102867190d76b421ba5cc5d55c9a62230caeaa
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-05-20 19:16:13 +0800

    in getall and getbyid func, added include func to include the appuser

--- backend/repositories/CommentRepository.cs
+++ backend/repositories/CommentRepository.cs
@@ -21,56 +21,61 @@ namespace backend.repositories
         {
             await _context.AddAsync(commentDto);
             await _context.SaveChangesAsync();
-            
+
             return commentDto;
         }
 
         public async Task<Comment?> DeleteCommentAsync(int id)
         {
             var comment = await _context.Comments.FirstOrDefaultAsync(c => c.Id == id);
-            
-            if(comment == null)
+
+            if (comment == null)
             {
                 return null;
             }
-            
+
             _context.Comments.Remove(comment);
             await _context.SaveChangesAsync();
-            
+
             return comment;
         }
 
         public async Task<Comment?> FindCommentAsync(int id)
         {
             var comment = await _context.Comments.FindAsync(id);
-            
-            if(comment == null)
+
+            if (comment == null)
             {
                 return null;
             }
-            
+
             return comment;
         }
 
         public async Task<List<Comment>> GetAllCommentsAsync()
         {
-            return await _context.Comments.ToListAsync();
+            return await _context.Comments.Include(a => a.AppUser).ToListAsync();
+        }
+
+        public async Task<Comment?> GetByIdAsync(int id)
+        {
+            return await _context.Comments.Include(a => a.AppUser).FirstOrDefaultAsync(c => c.Id == id);
         }
 
         public async Task<Comment?> UpdateCommentAsync(int id, Comment request)
         {
             var comment = await _context.Comments.FindAsync(id);
-            
-            if(comment == null)
+
+            if (comment == null)
             {
                 return null;
             }
-            
+
             comment.Title = request.Title;
             comment.Content = request.Content;
-            
+
             await _context.SaveChangesAsync();
-            
+
             return comment;
         }
     }
