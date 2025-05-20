95a7d2afbcfcb3fda0aa14efdc203c6df4b37485
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-05-20 19:14:35 +0800

    added CreatedBy

--- backend/dtos/Comment/CommentDto.cs
+++ backend/dtos/Comment/CommentDto.cs
@@ -11,6 +11,7 @@ namespace backend.dtos.Comment
         public string Title { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public string CreatedBy { get; set; } = string.Empty;
         public int? StockId { get; set; }
     }
 }
